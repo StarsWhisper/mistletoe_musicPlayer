4253ad91f63d85cc9e6f84a15775dc28121f1925
StarsWhisper <554809247@qq.com>
2015-04-08 15:54:22 +0800

    随意布局完成，基本功能完成，电话拨入时停止播放音乐，放歌时甩手机可以换歌

---
+++ src/com/mistletoe/musicplayer/aboutMusic/AlbumsActivity.java
@@ -0,0 +1,40 @@
+package com.mistletoe.musicplayer.aboutMusic;
+
+import com.mistletoe.musicplayer.adapter.AlbumsAdapter;
+import com.mistletoe.musicplayer.util.MusicList;
+import com.mistletoe_musicplayer.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class AlbumsActivity extends Activity {
+
+	private ListView albumListView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.albums);
+		
+		albumListView=(ListView) this.findViewById(R.id.albumListView);
+		AlbumsAdapter adapter=new AlbumsAdapter(this, MusicList.getMusicData(this));
+		albumListView.setAdapter(adapter);
+		albumListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(AlbumsActivity.this,
+						MusicActivity.class);
+				intent.putExtra("id", arg2);
+				startActivity(intent);
+			}
+		});
+	}
+}
---
+++ src/com/mistletoe/musicplayer/aboutMusic/ArtistsActivity.java
@@ -0,0 +1,40 @@
+package com.mistletoe.musicplayer.aboutMusic;
+
+import com.mistletoe.musicplayer.adapter.ArtistsAdapter;
+import com.mistletoe.musicplayer.util.MusicList;
+import com.mistletoe_musicplayer.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class ArtistsActivity extends Activity {
+   
+	private ListView artistListView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.artist);
+		
+		artistListView=(ListView) this.findViewById(R.id.artistListView);
+		ArtistsAdapter adapter=new ArtistsAdapter(this, MusicList.getMusicData(this));
+		artistListView.setAdapter(adapter);
+		artistListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(ArtistsActivity.this,
+						MusicActivity.class);
+				intent.putExtra("id", arg2);
+				startActivity(intent);
+			}
+		});
+	}
+}
